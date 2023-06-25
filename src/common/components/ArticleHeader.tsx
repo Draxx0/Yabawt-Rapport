5ca8d76bd872c6ea354aa78c854008218e513b00
DraxX0 <williamfort.lmgl@gmail.com>
2023-06-25 21:12:05 +0200

    :sparkles: add article header component

---
+++ src/common/components/ArticleHeader.tsx
@@ -0,0 +1,21 @@
+type Props = {
+ articleTitle: string;
+ articleSubtitle?: string;
+ articleFrom: string;
+ articleFromIcon: string;
+ articleFromUrl?: string;
+}
+
+const ArticleHeader = ({ articleTitle, articleFrom, articleFromIcon, articleFromUrl }: Props) => {
+ return (
+  <section className="container mx-auto pt-14 md:pt-28">
+   <h1 className="text-3xl font-bold pb-4">{articleTitle}</h1>
+   <div className="flex items-center gap-2">
+    <p className="text-gray-600">Un article-reportage de <a className="underline font-semibold" target="_blank" href={articleFromUrl}>{articleFrom}</a></p>
+    <img className="w-10 h-10 border-[1.5px] border-red object-cover rounded-full" src={articleFromIcon} alt="" />
+   </div>
+  </section>
+ );
+}
+
+export default ArticleHeader;
