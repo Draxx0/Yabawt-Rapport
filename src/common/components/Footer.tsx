260a3143623fc80bc0d1e31c22378d830012db49
DraxX0 <williamfort.lmgl@gmail.com>
2023-06-26 20:44:57 +0200

    :bug: fix footer index

--- src/common/components/Footer.tsx
+++ src/common/components/Footer.tsx
@@ -2,7 +2,7 @@ import Connect from "./Connect";
 
 const Footer = () => {
  return (
-  <footer className="bg-white py-12 border-gray-100 border-t-2 w-full z-50 shadow-sm">
+  <footer className="bg-white py-12 border-gray-100 border-t-2 w-full z-50 shadow-sm relative">
    <div className="container flex items-center justify-evenly">
     <p>Rapport de stage Yabawt - 2023</p>
     <Connect />
