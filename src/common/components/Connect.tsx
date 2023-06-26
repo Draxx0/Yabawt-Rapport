4cd7132a7d22882c17d54bc1772dceb55cb63e7c
DraxX0 <williamfort.lmgl@gmail.com>
2023-06-26 20:05:17 +0200

    :sparkles: add global component for connects link

---
+++ src/common/components/Connect.tsx
@@ -0,0 +1,13 @@
+import Icon from "./Icon";
+
+const Connect = () => {
+ return (
+  <div className="hidden items-center gap-4 md:flex">
+   <Icon name="website" url="https://william-fort.fr" />
+   <Icon name="linkedin" url="https://www.linkedin.com/in/william-fort/" />
+   <Icon name="github" url="https://github.com/Draxx0" />
+  </div>
+ );
+}
+
+export default Connect;
