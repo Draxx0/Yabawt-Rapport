a1e5d9b6d9c56f7a8a5ac0f3f8c2c9c570f51dc3
DraxX0 <williamfort.lmgl@gmail.com>
2023-06-26 20:05:49 +0200

    :sparkles add footer

---
+++ src/common/components/Footer.tsx
@@ -0,0 +1,14 @@
+import Connect from "./Connect";
+
+const Footer = () => {
+ return (
+  <footer className="bg-white py-12 border-gray-100 border-t-2 w-full z-50 shadow-sm">
+   <div className="container flex items-center justify-evenly">
+    <p>Rapport de stage Yabawt - 2023</p>
+    <Connect />
+   </div>
+  </footer>
+ );
+}
+
+export default Footer;
