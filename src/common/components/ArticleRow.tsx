04bbd55e75c89e6dbd20957f5f00a4aeade9440c
DraxX0 <williamfort.lmgl@gmail.com>
2023-06-26 20:05:00 +0200

    :iphone: add responsive behaviour on article components

--- src/common/components/ArticleRow.tsx
+++ src/common/components/ArticleRow.tsx
@@ -1,7 +1,7 @@
 import Button from "./Button";
 
 const ArticleRowText = ({ children, articleSubtitle, hasButton = false, textButton = "Découvrir" }: { children: React.ReactElement, articleSubtitle: string; hasButton?: boolean, textButton?: string; }) => (
- <div className="flex flex-col gap-6 w-1/2">
+ <div className="flex flex-col gap-6 w-full md:w-1/2">
   <h2 className="text-2xl font-bold">{articleSubtitle}</h2>
   {children}
   {(hasButton && textButton) && (
@@ -11,7 +11,7 @@ const ArticleRowText = ({ children, articleSubtitle, hasButton = false, textButt
 );
 
 const ArticleRowImage = ({ children, imageUrl }: { children: React.ReactElement, imageUrl: string; }) => (
- <div className="flex flex-col gap-3 w-1/2">
+ <div className="flex flex-col gap-3 w-full md:w-1/2">
   <div className="flex justify-center">
    <div className="relative">
     <img src={imageUrl} alt="" className="max-h-[32rem] object-cover rounded-md" />
@@ -22,7 +22,7 @@ const ArticleRowImage = ({ children, imageUrl }: { children: React.ReactElement,
 );
 
 const ArticleRow = ({ sectionName, children }: { sectionName: string; children: React.ReactElement }) => (
- <section className="container py-14 md:py-28 flex space-between gap-10" id={sectionName}>
+ <section className="container pt-14 md:pt-28 flex space-between gap-20 md:gap-10 flex-col-reverse md:flex-row" id={sectionName}>
   {children}
  </section>
 );
