648481a092b25994a5c14486c9df777a26edb95d
DraxX0 <williamfort.lmgl@gmail.com>
2023-06-26 20:03:11 +0200

    :lipstick: change container max-w & add padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ export default {
   theme: {
     container: {
       center: true,
+      padding: "2rem",
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1200px",
+      },
     },
     extend: {
       fontFamily: {
