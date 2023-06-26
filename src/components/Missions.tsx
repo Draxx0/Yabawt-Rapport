b4c45c48b301bc0d6029b347bc6c553c35ad3062
DraxX0 <williamfort.lmgl@gmail.com>
2023-06-26 20:05:40 +0200

    :construction: missions section WIP

---
+++ src/components/Missions.tsx
@@ -0,0 +1,10 @@
+const Missions = () => {
+ return (
+  <section className="container pt-14 md:pt-28">
+   <h2 className="text-2xl font-bold mb-4">Mes missions</h2>
+   <p className="font-medium">J’ai au cours de mon stage pu intervenir sur deux projets, tous deux étaient montés en NestJS Typescript pour le back et React Typescript pour le front. J’ai donc, lors des missions que j’ai pu réaliser  sur ces deux projets, travailler en frontend et en backend, j’ai eu l’occasion de toucher à tout !</p>
+  </section>
+ );
+}
+
+export default Missions;
