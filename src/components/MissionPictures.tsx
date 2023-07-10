f663e1c0a43d9d14e8a1ae9f873e26a7dbe3bf37
William_Fort <william.fort@yabawt.com>
2023-07-10 16:21:28 +0200

    :bug: fix somes little bugs

--- src/components/MissionPictures.tsx
+++ src/components/MissionPictures.tsx
@@ -22,7 +22,7 @@ const MissionPictures = ({ pictures }: { pictures: Array<{ path: string }> }) =>
         >
           {pictures.map((picture, index) => (
             <SwiperSlide className="w-full">
-              <img src={picture.path} onClick={() => window.open(`https://yabawt-rapport.william-fort.fr/${picture.path}`)} alt="" key={index} className=" h-64 cursor-zoom-in w-full object-cover transition ease-in-out duration-300 border-transparent border-2 rounded hover:border-red" />
+              <img src={picture.path} onClick={() => window.open(`https://yabawt-rapport.william-fort.fr${picture.path}`)} alt="" key={index} className=" h-64 cursor-zoom-in w-full object-cover transition ease-in-out duration-300 border-transparent border-2 rounded hover:border-red" />
             </SwiperSlide>
           ))}
           <div className="absolute z-20 left-5 top-1/2 -translate-y-1/2 image-swiper-button-prev bg-red rounded-full p-1">
