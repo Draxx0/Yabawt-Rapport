0c449fac91c120e02b2fe1c70ff85224867cd79b
William_Fort <william.fort@yabawt.com>
2023-07-10 16:03:31 +0200

    :sparkles: add swiper on missions pictures

--- src/components/MissionPictures.tsx
+++ src/components/MissionPictures.tsx
@@ -1,21 +1,40 @@
-import { useState } from "react";
-import Modal from "./common/Modal";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import Icon from "./common/Icon";
 
 const MissionPictures = ({ pictures }: { pictures: Array<{ path: string }> }) => {
- const [currentImageUrl, setCurrentImageUrl] = useState<string>("");
- console.log("CURRENT IMAGE", currentImageUrl)
- return (
-  <div className="mt-16">
-   <p className="font-semibold text-lg">Quelques images</p>
-   <div className="flex gap-10 flex-wrap mt-10">
-    {pictures.map((picture, index) => (
-     <img src={picture.path} alt="" onClick={() => setCurrentImageUrl(picture.path)} key={index} className="w-48 object-cover cursor-zoom-in transition ease-in-out duration-300 border-transparent border-2 rounded hover:border-red hover:-translate-y-1" />
-    ))}
-   </div>
-
-   <Modal isOpen={!!currentImageUrl} path={currentImageUrl} handleCloseModal={() => setCurrentImageUrl("")} />
-  </div>
- );
+  return (
+    <div className="mt-16">
+      <p className="font-semibold text-lg">Quelques images</p>
+      <div className="mt-10 select-none">
+        <Swiper
+          className="relative"
+          modules={[Navigation]}
+          slidesPerView={2}
+          navigation={{
+            nextEl: ".image-swiper-button-next",
+            prevEl: ".image-swiper-button-prev",
+          }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          {pictures.map((picture, index) => (
+            <SwiperSlide className="w-full">
+              <img src={picture.path} onClick={() => window.open(`https://yabawt-rapport.william-fort.fr/${picture.path}`)} alt="" key={index} className=" h-64 cursor-zoom-in w-full object-cover transition ease-in-out duration-300 border-transparent border-2 rounded hover:border-red" />
+            </SwiperSlide>
+          ))}
+          <div className="absolute z-20 left-5 top-1/2 -translate-y-1/2 image-swiper-button-prev bg-red rounded-full p-1">
+            <Icon name="arrow" className="text-white rotate-180" />
+          </div>
+          <div className="absolute z-20 right-5 top-1/2 -translate-y-1/2 image-swiper-button-next bg-red rounded-full p-1">
+            <Icon name="arrow" className="text-white" />
+          </div>
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 
 export default MissionPictures;
