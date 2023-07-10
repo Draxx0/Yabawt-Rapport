185e247e9f46bcd3073a851391a641ac581c0807
William_Fort <william.fort@yabawt.com>
2023-07-10 14:47:46 +0200

    :art: improve code visibility using clsx

--- src/components/common/ReturnTop.tsx
+++ src/components/common/ReturnTop.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import useDetectBottomPage from "../../hook/useDetectBottomPage";
 import Icon from "./Icon";
 
@@ -5,10 +6,13 @@ const ReturnTop = () => {
 
   const isDisplay = useDetectBottomPage()
 
-  console.log(isDisplay)
+  const classes = clsx({
+    "visible -translate-y-3": isDisplay,
+    "invisible pointer-events-none": !isDisplay
+  }, "transition ease-in-out duration-100 fixed bottom-10 right-10 bg-red rounded-full w-10 h-10 z-[100]")
 
   return (
-    <div className={`${isDisplay ? "visible -translate-y-3" : "invisible pointer-events-none"} transition ease-in-out duration-100 fixed bottom-10 right-10 bg-red rounded-full w-10 h-10 z-[100]`}>
+    <div className={classes}>
       <div className="relative flex justify-center items-center w-full h-full -rotate-90" onClick={() => window.scrollTo(0, 0)}>
         <Icon name="arrow" />
       </div>
