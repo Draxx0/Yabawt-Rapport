e72960a2e4a7dae53922606efd98ccaeade54f69
DraxX0 <williamfort.lmgl@gmail.com>
2023-07-24 12:04:39 +0200

    :sparkles: add reviews & :fire: delete unused code

--- src/components/common/Connect.tsx
+++ src/components/common/Connect.tsx
@@ -3,9 +3,9 @@ import Icon from "./Icon";
 const Connect = () => {
  return (
   <div className="hidden items-center gap-4 md:flex">
-   <Icon name="website" url="https://william-fort.fr" />
-   <Icon name="linkedin" url="https://www.linkedin.com/in/william-fort/" />
-   <Icon name="github" url="https://github.com/Draxx0" />
+   <Icon name="website" url="https://william-fort.fr" className="hover:-rotate-12 hover:scale-110 transition ease-in duration-200" />
+   <Icon name="linkedin" url="https://www.linkedin.com/in/william-fort/" className="hover:-rotate-12 hover:scale-110 transition ease-in duration-200" />
+   <Icon name="github" url="https://github.com/Draxx0" className="hover:-rotate-12 transition hover:scale-110 ease-in duration-200" />
   </div>
  );
 }
--- src/hook/useDetectModal.ts
+++
@@ -1,9 +0,0 @@
-const useDetectModal = ({ isOpen }: { isOpen: boolean }) => {
-  if (isOpen) {
-    document.body.style.overflowY = "hidden";
-  } else {
-    document.body.style.overflowY = "visible";
-  }
-};
-
-export default useDetectModal;
